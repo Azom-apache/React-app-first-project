1d5ab5ce6e01eda97fd11fe3a4928cd741cf565b
Azom-apache <azomsarker217@gmail.com>
2023-09-13 21:22:40 +0600

    first commit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,30 @@
+import React, { useState, useEffect } from 'react';
 
-function ListGroup(){
-    const items = ["azom", "salma"];
-return (
+function ListGroup() {
+  const [countries, setCountries] = useState([]);
+  const textAlign = {
+    textAlign: 'center',
+  };
+
+  useEffect(() => {
+    // Fetch data from the URL when the component mounts
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-        {items.map((item)=>(<li key={item}>{item}</li>))}
-        </ul>
+      <div style={textAlign}>List</div>
+      <ul style={textAlign} className="list-group">
+
+        {countries.map((country) => (
+          <li key={country.name.common}>{country.name.common}</li>
+        ))}
+        
+      </ul>
     </>
-);
+  );
 }
+
 export default ListGroup;
